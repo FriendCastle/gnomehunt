683715359a2fcdb53c02cc26269387f6ff75898d
Andrew Chau <chau.andrew.s@gmail.com>
2024-02-10 19:38:22 -0800

    setup reticle placement script

---
+++ GnomeHunt/Assets/Scripts/Reticle.cs
@@ -0,0 +1,68 @@
+using Niantic.Lightship.AR.NavigationMesh;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+	[SerializeField] private Camera arCamera = null;
+
+	[SerializeField] private LightshipNavMeshManager navMeshManager = null;
+
+	[SerializeField] private GameObject reticlePrefab = null;
+
+	[SerializeField] private float reticleDistance = 2f;
+
+	[SerializeField] private float reticleHeightAboveGround = .1f;
+
+	[SerializeField] private GameObject objectToPlace = null;
+
+	private GameObject reticleInstance;
+
+	private float currentReticleHeight = 0;
+
+	void Start()
+	{
+		if (reticlePrefab != null)
+		{
+			reticleInstance = Instantiate(reticlePrefab);
+			reticleInstance.SetActive(false);
+		}
+	}
+	
+	void Update()
+	{
+		if (navMeshManager != null)
+		{
+			LightshipNavMesh navMesh = navMeshManager.LightshipNavMesh;
+
+			Ray ray = new Ray(arCamera.transform.position, arCamera.transform.forward);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, reticleDistance))
+			{
+				if (reticleInstance != null && reticleInstance.activeSelf == false)
+				{
+					reticleInstance.SetActive(true);
+				}
+
+				currentReticleHeight = hit.point.y;
+			}
+		}
+
+		UpdateReticlePosition();
+
+		if (Input.GetMouseButtonDown(0) && reticleInstance != null && reticleInstance.activeSelf)
+		{
+			Instantiate(objectToPlace, reticleInstance.transform.position, Quaternion.identity);
+		}
+	}
+
+	void UpdateReticlePosition()
+	{
+		if (reticleInstance != null)
+		{
+			Vector3 newPosition = arCamera.transform.position + arCamera.transform.forward * reticleDistance;
+			newPosition.y = currentReticleHeight + reticleHeightAboveGround;
+			reticleInstance.transform.position = newPosition;
+		}
+	}
+}
